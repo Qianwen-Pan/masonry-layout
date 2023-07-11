7ca7719cf01b7a0efa4f6e8c31a4d42f2b2efb81
qianwen_pan <qianwen.pan2021@gmail.com>
2023-07-11 11:07:55 -0400

    Version 2: use react-masonry-css library

--- src/MasonryGrid.js
+++ src/MasonryGrid.js
@@ -1,9 +1,11 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 import "./MasonryGrid.css"
+import Masonry from "react-masonry-css";
 
 function MasonryGrid() {
   const [pins, setPins] = useState([]);
+  
 
   useEffect(() => {
     async function fetchData() {
@@ -14,16 +16,26 @@ function MasonryGrid() {
     fetchData();
   }, []);
 
-  return (
-    <div className="masonry">
-      {pins.map((pin) => (
+  const breakpointColumnsObj = {
+    default: 4,
+    1100: 3,
+    700: 2,
+    500: 1
+  };
 
-        <div className="masonry-item" key={pin.id}>
-          <img src={pin.image} alt={pin.title} />
-          <p>{pin.title}</p>
-        </div>
-      ))}
-    </div>
+
+  return (
+    <Masonry
+        breakpointCols={breakpointColumnsObj}
+        className="my-masonry-grid"
+        columnClassName="my-masonry-grid_column">
+        {pins.map((pin) => (
+          <div key={pin.id}>
+            <img src={pin.image} alt={pin.title} />
+            <p>{pin.title}</p>
+          </div>
+        ))}
+      </Masonry>
   );
 }
 export default MasonryGrid;
